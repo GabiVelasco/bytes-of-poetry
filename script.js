e100b49ae0bcd41a023e37772497bf314d1ff75b
GabrielaVelasco <gabriela.luppe@gmail.com>
2023-07-23 11:06:55 +0200

    organising index html with java script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-https://cdnjs.cloudflare.com/ajax/libs/angular/12.2.17/core.umd.min.js
+// https://cdnjs.cloudflare.com/ajax/libs/angular/12.2.17/core.umd.min.js
 
-
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 
 
